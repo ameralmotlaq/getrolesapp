3ae6aafb00af5ec6fe7f875351d4cc8bb1278741
ameralmotlaq <100343177+ameralmotlaq@users.noreply.github.com>
2022-10-07 14:44:14 +0300

    Update index.js

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -6,12 +6,17 @@ const roleGroupMappings = {
     'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
 };
 
+const appRolesMappings = {
+    'Contributors': 'ff16315c-5438-44d1-84b0-3dd7bfd506ff',
+    'Viewers': '96113c40-6a7a-4999-a048-184a6aee5b37'
+};
+
 module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = [];
     
-    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
-        if (await isUserInGroup(groupId, user.accessToken)) {
+    for (const [role, roleId] of Object.entries(appRolesMappings)) {
+        if (await isUserInRole(roleId, user.accessToken)) {
             roles.push(role);
         }
     }
@@ -19,6 +24,25 @@ module.exports = async function (context, req) {
     context.res.json({
         roles
     });
+}
+
+async function isUserInRole(roleId, bearerToken) {
+    const url = new URL('https://graph.microsoft.com/v1.0/me/appRoleAssignments');
+    url.searchParams.append('$filter', `appRoleId eq '${roleId}'`);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const graphResponse = await response.json();
+    const matchingGroups = graphResponse.value.filter(roleAssignment => roleAssignment.appRoleId === roleId);
+    return matchingGroups.length > 0;
 }
 
 async function isUserInGroup(groupId, bearerToken) {
